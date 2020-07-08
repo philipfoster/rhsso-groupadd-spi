62a8c782a6e5c014c2fd976a22a62d7c0abdf9e8
Philip <pfoster@redhat.com>
2020-07-08 13:05:59 -0500

    Add code to externalize group lookups

---
+++ src/test/java/com/raytheon/sso/GroupLookupServiceTest.java
@@ -0,0 +1,28 @@
+package com.raytheon.sso;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import java.util.Properties;
+import org.junit.jupiter.api.Test;
+
+class GroupLookupServiceTest {
+
+    private static final String[] TYPE_HOLDER = new String[0];
+
+    @Test
+    void getGroupsForEmail() {
+        Properties props = new Properties();
+        props.put("website.gov", "Group1 ");
+        props.put("website.mil", " Group1,  Group2 ");
+
+        GroupLookupService svc = new GroupLookupService(props);
+        List<String> govDomain = svc.getGroupsForEmailDomain("john.doe@website.gov");
+        List<String> milDomain = svc.getGroupsForEmailDomain("john.doe@website.mil");
+
+        assertArrayEquals(new String[] {"Group1"}, govDomain.toArray(TYPE_HOLDER));
+        assertArrayEquals(new String[] {"Group1", "Group2"}, milDomain.toArray(TYPE_HOLDER));
+        assertThrows(IllegalArgumentException.class, () -> svc.getGroupsForEmailDomain("not an email address"));
+    }
+}
